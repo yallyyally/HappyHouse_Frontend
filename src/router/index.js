49d8de01144d708b757050b130f37da3326ff2aa
노효진 <23gywls@naver.com>
2021-05-21 13:30:54 +0900

    member 기초작업

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import join from "../views/member/join.vue";
+import login from "../views/member/login.vue";
+import mypage from "../views/member/mypage.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +21,21 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/join",
+    name :"join",
+    component: join,
+  },
+  {
+    path: "/login",
+    name :"login",
+    component: login,
+  },
+  {
+    path: "/mypage",
+    name :"mypage",
+    component: mypage,
   },
 ];
 
