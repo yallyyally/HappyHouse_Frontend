5e81cacd3b08a04a63f687be1e212a9c8cbb0ebe
yallyyally <jhshin0028@gmail.com>
2021-05-21 11:28:05 +0900

    ~네비게이션바
    .css.....

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,98 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <!-- 네비 테마 -->
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <!-- 왼쪽: 로고, 커뮤니티(공지사항,큐앤에이,자유게시판),매물,동네정보\-->
+    <!-- 로고 -->
+    <b-navbar-brand href="#">
+      <img src="../src/assets/happyhouse_logo.png" id="logo"/>
+    </b-navbar-brand>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- 커뮤니티 -->
+            <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="커뮤니티" right>
+          <b-dropdown-item href="#">자유게시판</b-dropdown-item>
+          <b-dropdown-item href="#">Q&A</b-dropdown-item>
+          <b-dropdown-item href="#">공지사항</b-dropdown-item>
+         </b-nav-item-dropdown>
+
+<!-- 있어보이는 단어로 바꿀 것.. -->
+      <!-- 매물 -->
+      <b-navbar-nav>
+        <b-nav-item href="#">매물정보</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- 동네정보 -->
+      <b-navbar-nav>
+        <b-nav-item href="#">동네정보</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar-nav>
+    
+      <!-- 오른쪽: 로그인/회원가입/로그아웃,마이페이지 -->
+  
+  <!-- 로그인 전 보이는 버전 -->
+      <b-navbar-nav id="beforeLogin">
+      <!-- 로그인 아이디, 비밀번호 입력 -->
+        <b-form>
+          <!-- 아이디 -->
+          <!-- 비밀번호 -->
+          <input type="text" style="margin-top:7px;border:none;" size="sm" placeholder="ID"/>&nbsp;
+          <input style="margin-top:7px;border:none;" type="password" size="sm" placeholder="Password"/>&nbsp;
+          <!-- 로그인 버튼 -->
+          
+        <b-button size="sm" type="submit" style="margin-bottom:5px;">Login</b-button>
+        </b-form>
+
+      <!-- 회원가입 -->
+      <b-navbar-nav>
+        <b-nav-item href="#">회원가입</b-nav-item>
+      </b-navbar-nav>
+        
+        </b-navbar-nav>
+    <!-- 로그인 후 보이는 버전 -->
+          <b-navbar-nav id="afterLogin">
+          <!-- 로그아웃 -->
+                <b-navbar-nav>
+        <b-nav-item href="#">로그아웃</b-nav-item>
+      </b-navbar-nav>
+          <!-- 마이페이지 -->
+      <b-navbar-nav>
+        <b-nav-item href="#">마이페이지</b-nav-item>
+      </b-navbar-nav>
+        
+        </b-navbar-nav>
+
+
+    </b-collapse>
+  </b-navbar>
+      <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+  <b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+    <b-card-title>뿡트스트랩</b-card-title> -->
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
+#logo{
+  width:150px;
+  margin-left: 50px;
+}
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
+*
 #nav {
   padding: 30px;
 }
@@ -28,5 +104,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// bootstrap-vue 추가
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
