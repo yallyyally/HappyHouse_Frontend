15e0df699545fdbdf6f34e4ed604ae15a42445b0
노효진 <23gywls@naver.com>
2021-05-22 19:14:59 +0900

    토큰 기초작업

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios';
 import http from "@/util/http-commons";
 
 Vue.use(Vuex);
 
-// const SERVER_URL = process.env.VUE_APP_SERVER_URL;
+const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
 export default new Vuex.Store({
   state: {
-    // acceccToken: null,
-    // userId: "",
-    // userName: "",
+    acceccToken: null,
+    userId: '',
+    userName: '',
     houses: [],
     optionsGu: [],
   },
@@ -22,18 +23,18 @@ export default new Vuex.Store({
       console.log("=====get 구 목록=======");
       return state.optionsGu;
     },
-    // getAccessToken(state) {
-    //   if (state.accessToken !== null) return state.accessToken;
-    //   return localStorage.accessToken;
-    // },
-    // getUserId(state) {
-    //   if (state.userId !== "") return state.userId;
-    //   return localStorage.userId;
-    // },
-    // getUserName(state) {
-    //   if (state.userName !== "") return state.userName;
-    //   return localStorage.userName;
-    // },
+    getAccessToken(state) {
+      if (state.accessToken !== null) return state.accessToken;
+      return localStorage.accessToken;
+    },
+    getUserId(state) {
+      if (state.userId !== "") return state.userId;
+      return localStorage.userId;
+    },
+    getUserName(state) {
+      if (state.userName !== "") return state.userName;
+      return localStorage.userName;
+    },
   },
   mutations: {
     setHouses(state, payload) {
@@ -51,22 +52,22 @@ export default new Vuex.Store({
       state.optionsGu = payload;
       console.log("=======비동기 통신 완료/구목록==========");
     },
-    // LOGIN(state, payload) {
-    //   state.accessToken = payload["auth-token"];
-    //   state.userId = payload["user-id"];
-    //   state.userName = payload["user-name"];
-    //   localStorage.accessToken = payload["auth-token"];
-    //   localStorage.userId = payload["user-id"];
-    //   localStorage.userName = payload["user-name"];
-    // },
-    // LOGOUT(state) {
-    //   state.accessToken = null;
-    //   delete localStorage.accessToken;
-    //   delete localStorage.userId;
-    //   delete localStorage.userName;
-    //   state.userId = "";
-    //   state.userName = "";
-    // },
+    LOGIN(state, payload) {
+      state.accessToken = payload["auth-token"];
+      state.userId = payload["user-id"];
+      state.userName = payload["user-name"];
+      localStorage.accessToken = payload["auth-token"];
+      localStorage.userId = payload["user-id"];
+      localStorage.userName = payload["user-name"];
+    },
+    LOGOUT(state) {
+      state.accessToken = null;
+      delete localStorage.accessToken;
+      delete localStorage.userId;
+      delete localStorage.userName;
+      state.userId = '';
+      state.userName = '';
+    },
   },
   actions: {
     getMap() {
@@ -95,24 +96,24 @@ export default new Vuex.Store({
         commit("setHouses", resp.data);
       });
     },
-    //   LOGIN(context, user) {
-    //     return axios
-    //       .post(`${SERVER_URL}/user/confirm/login`, user)
-    //       .then((response) => {
-    //         if (response.data.message == "로그인 실패") {
-    //           return "fail";
-    //         } else {
-    //           context.commit("LOGIN", response.data);
-    //           axios.defaults.headers.common["auth-token"] = `${response.data["auth-token"]}`;
-    //           return "success";
-    //         }
-    //       })
-    //       .catch(({ message }) => alert(message));
-    //   },
-    //   LOGOUT(context) {
-    //     axios.defaults.headers.common["auth-token"] = undefined;
-    //     context.commit("LOGOUT");
-    //   },
+      LOGIN(context, user) {
+        return axios
+          .post(`${SERVER_URL}/member/confirm/login`, user)
+          .then((response) => {
+            if (response.data.message == "로그인 실패") {
+              return "fail";
+            } else {
+              context.commit("LOGIN", response.data);
+              axios.defaults.headers.common["auth-token"] = `${response.data["auth-token"]}`;
+              return "success";
+            }
+          })
+          .catch(({ message }) => alert(message));
+      },
+      LOGOUT(context) {
+        axios.defaults.headers.common["auth-token"] = undefined;
+        context.commit("LOGOUT");
+      },
   },
   modules: {},
 });
