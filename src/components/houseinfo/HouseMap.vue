60d0b6d70c4d5a6d2f00d183d0f5908f0c497cf5
yallyyally <jhshin0028@gmail.com>
2021-05-24 20:14:14 +0900

    갑자기 지도 성공;

--- src/components/houseinfo/HouseMap.vue
+++ src/components/houseinfo/HouseMap.vue
@@ -1,25 +1,19 @@
 <template>
-<div>
-  here is map
-  <button @click="what">show map</button>
-  <div id="map" style="width:100%;height:400px;"></div>
-  end of map
-</div>
+
+  <div class="content">
+    <div id="map" style="width:100%;height:400px;"></div>
+  </div>
 </template>
+
 <script>
 export default {
   mounted () {
-    
-  },
-  methods:{
-    what(){
-      alert('???');
-var mapOptions = {
+    var mapOptions = {
       center: new naver.maps.LatLng(37.3595704, 127.105399),
       zoom: 10
     };
 
     var map = new naver.maps.Map('map', mapOptions);
-    }
   }
 }
+</script>
