6d08f00281fd977d0ec9e55de88ec54921d83284
노효진 <23gywls@naver.com>
2021-05-24 22:19:59 +0900

    메인수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="content">
-    <div id="map" style="width:100%;height:400px;"></div>
+    <!-- <div id="map" style="width:100%;height:400px;"></div> -->
     <v-container fluid>
       <v-row class="mt-16 pt-7" justify="center">
         <v-img
@@ -18,14 +18,14 @@
 </template>
 
 <script>
-export default {
-  mounted () {
-    var mapOptions = {
-      center: new naver.maps.LatLng(37.3595704, 127.105399),
-      zoom: 10
-    };
+// export default {
+//   mounted () {
+//     var mapOptions = {
+//       center: new naver.maps.LatLng(37.3595704, 127.105399),
+//       zoom: 10
+//     };
 
-    var map = new naver.maps.Map('map', mapOptions);
-  }
-}
+//     var map = new naver.maps.Map('map', mapOptions);
+//   }
+// }
 </script>
