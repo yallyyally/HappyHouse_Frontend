49d9ee677c2059dc82b7a1eae7bd5fc38f89031d
노효진 <23gywls@naver.com>
2021-05-24 14:38:41 +0900

    메인화면 수정

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
+<v-app>
   <div id="app">
 
-<app-header></app-header>
+    <app-header></app-header>
     <!-- 라우터 뷰 -->
     <router-view />
-
-    <!-- header 코드가 길어서 footer는 따로 뺏는데  -->
-    <!-- 시간이 남으면 header 코드도 빼면 좋을듯함니다 ~~! -->
+    
     <app-footer></app-footer>
 
   </div>
+</v-app>
 </template>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <hello-world />
+  <div class="content">
+    <v-container fluid>
+      <v-row class="mt-16 pt-7" justify="center">
+        <v-img
+          position="center"
+          lazy-src="@/assets/happyhouse_logo.png"
+          max-height="500"
+          max-width="500"
+          src="@/assets/logo.png"
+        ></v-img>
+      </v-row>
+      <br />
+    </v-container>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
 </script>
